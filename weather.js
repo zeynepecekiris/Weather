d6b60def9619b74eb6edea426961d3853ac72144
zeynepecekiris <zeynep.ece933@gmail.com>
2024-10-28 18:07:21 +0300

    bitmedi bu yapacak bisi yok

--- weather.js
+++ weather.js
@@ -1,36 +1,48 @@
-const apiKey = 'd579f439a6098e38badd0cc655ad4f02';
+const apikey = '74d5db86d1bc7b7f9917c66839e50cad';
 const form = document.getElementById('form');
 const city = document.getElementById('city');
 const weatherDiv = document.getElementById('weather');
 const iconDiv = document.getElementById('icon');
-const temperatureDiv  = document.getElementById('temperature');
-const DescriptionDiv = document.getElementById('Description');
-const DetailsDiv = document.getElementById('Details');
-
-
+const temperatureDiv = document.getElementById('temperature');
+const descriptionDiv = document.getElementById('description');
+const detailsDiv = document.getElementById('details');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const cityValue = city.value;
-    getWeather(cityValue);
+  e.preventDefault();
+  const cityValue = city.value;
+  getWeather(cityValue);
 });
 
-async function getWeather(cityValue){
-   
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`);
-        
-      
-        
-        const data = await response.json();
-        console.log(data);
-        
-        const temparature = Math(data.main.temp);
-        const icon = data.weather[0].icon;
-        const details = [
-            `Hissedilen: ${Math.round(data.main.feels_like)}`,
-             `Nem Orani: ${data.main.humidity}`,
-             `Ruzgar: ${data.wind.speed} m/s`,
+async function getWeather(cityValue) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`
+    );
 
-        ]
+    const data = await response.json();
+    console.log(data);
 
+    const temparature = Math.round(data.main.temp);
+    const icon = data.weather[0].icon;
+    const details = [
+      `Hissedilen: ${Math.round(data.main.feels_like)}`,
+      `Nem Oranı:${data.main.humidity}%`,
+      `Rüzgar:${data.wind.speed} m/s`,
+    ];
+
+    iconDiv.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
+    temperatureDiv.textContent = `${temparature}°C`;
+
+    let detailsNew = details.map((detail) => `<div>${detail}</div>`);
+    console.log(detailsNew);
+    descriptionDiv.textContent = '';
+
+    detailsDiv.innerHTML = detailsNew;
+  } catch (error) {
+    iconDiv.innerHTML = '';
+    temperatureDiv.textContent = '';
+    descriptionDiv.textContent = 'Lütfen geçerli bir şehir giriniz';
+    detailsDiv.innerHTML = '';
+  }
 }
+    
